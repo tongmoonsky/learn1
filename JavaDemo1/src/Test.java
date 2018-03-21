2b3877e91a04b8edb77bb6c9796ffff0d39b16bb
TONGMOON\彤彤 <986098090>
2018-03-21 15:07:33 +0800

    比较print与println的区别

---
+++ JavaDemo1/src/Test.java
@@ -0,0 +1,17 @@
+/**
+ * 2018.3.21
+ * @author 彤彤
+ *比较println与print的区别:换行
+ */
+public class Test {
+public static void main(String[] args) {
+	System.out.println("明天，");
+	System.out.println("你好");
+	
+	
+	System.out.print("明天，");
+	System.out.println("你好");
+	
+}
+
+}
